603f1cf8a605bd123bc1d301ffb4e091c240c09b
Oli Wright <oli@floor4.co.uk>
2021-10-03 18:46:36 +0100

    Exponential fan curve

--- fancontrol.py
+++ fancontrol.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from gpiozero import PWMLED, CPUTemperature, Button
 import logging
+import math
 import time
 
 logging.basicConfig(
@@ -12,8 +13,25 @@ FAN_PIN='GPIO13'
 FAN_FREQ=25
 TACH_PIN='GPIO6'
 
+FAN_OFF_THRESHOLD=43
+FAN_MAX_THRESHOLD=55
+
+FAN_LOW_LIMIT=0.05
+FAN_POINT_COUNT=30
+
+FAN_SET_POINTS=[FAN_LOW_LIMIT + ((1.-FAN_LOW_LIMIT)*(float(x)/FAN_POINT_COUNT)) for x in range(1,FAN_POINT_COUNT+1)]
+
 rpm = 0
 t = time.time()
+
+def get_speed(temperature):
+    if temperature <= FAN_OFF_THRESHOLD:
+        return 0.
+    elif temperature >= FAN_MAX_THRESHOLD:
+        return 1.
+    else:
+        index = int(math.floor((FAN_POINT_COUNT-1) * ((temperature - FAN_OFF_THRESHOLD)/(FAN_MAX_THRESHOLD - FAN_OFF_THRESHOLD))))
+        return FAN_SET_POINTS[index]
 
 def tick():
     global rpm
@@ -31,8 +49,8 @@ tach.when_pressed = tick
 
 try:
     while True:
+        print(get_speed(cpu.temperature))
         logging.info("CPU: {0}°C Fan Speed: {1}% RPM: {2}".format(round(cpu.temperature, 1), fan.value * 100., round(rpm, 1)))
-
         time.sleep(5)
 
 except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt
